6a2ca8b7905663c2a050433cc53285c676af46f9
xibn <kaptenseo08@gmail.com>
2025-05-28 15:54:16 +0700

    Delete indofragrances directory

--- indofragrances/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center' }}>
-      <h1>Welcome to IndoFragrances</h1>
-      <p>The Fragrance Shop Inc</p>
-    </div>
-  );
-}
