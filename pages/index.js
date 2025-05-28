6a0ef9ed16b2c0ed99d96df4588a5795a0ef0dad
xibn <kaptenseo08@gmail.com>
2025-05-28 15:55:09 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Welcome to IndoFragrances</h1>
+      <p>The Fragrance Shop Inc</p>
+    </div>
+  );
+}
