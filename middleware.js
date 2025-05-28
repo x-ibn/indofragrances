eb2e7905af3e2897c2b060359083bc527cd4d5fd
xibn <kaptenseo08@gmail.com>
2025-05-28 16:21:16 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,16 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function middleware(request) {
   const country = request.geo?.country || 'UNKNOWN';
   const ua = request.headers.get('user-agent') || '';
   const isBot = /bot|crawl|preview|facebook|google|whatsapp/i.test(ua);
 
-  // Bot atau user non-Indonesia → white.html
   if (isBot || country !== 'ID') {
     const url = request.nextUrl.clone();
     url.pathname = '/white.html';
     return NextResponse.rewrite(url);
   }
 
-  // Akses root oleh orang Indonesia → redirect ke landingpage
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/landingpage', request.url));
   }
